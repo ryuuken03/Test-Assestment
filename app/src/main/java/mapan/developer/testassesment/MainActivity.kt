ee7b29ba9347a5341f86fdd31c0ba352474d9792
= <=>
2024-01-10 16:58:28 +0700

    delete comment in MainActivity

--- app/src/main/java/mapan/developer/testassesment/MainActivity.kt
+++ app/src/main/java/mapan/developer/testassesment/MainActivity.kt
@@ -60,19 +60,3 @@ fun SetStatusBarColor(color: Color) {
         )
     }
 }
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    TestAssesmentTheme {
-//        Greeting("Android")
-//    }
-//}
